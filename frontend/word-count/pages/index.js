f244c6a2c9312862ef3d4bfbf2fdee33c9600ee7
Jonghyeok K <jka236@sfu.ca>
2022-06-16 22:24:02 -0700

    Minor text change

--- frontend/word-count/pages/index.js
+++ frontend/word-count/pages/index.js
@@ -1,13 +1,26 @@
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Link from "../src/Link";
 import React, { useEffect, useState } from "react";
 import BarChart from "../components/barChart";
-import DatePicker from "../components/datePicker";
 
 const Submission = ({ words }) => {
+  function formatDate(d) {
+    var month = "" + (d.getMonth() + 1);
+    var day = "" + d.getDate();
+    var year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+  }
+
+  var weekAgo = new Date(new Date().setDate(new Date().getDate() - 7));
   var current_date = new Date();
+
+  var today = formatDate(current_date);
+  var weekAgo = formatDate(weekAgo);
 
   // get total days between two dates
   var days = Math.floor(current_date / 86400000);
@@ -18,13 +31,17 @@ const Submission = ({ words }) => {
         <Container maxWidth="sm">
           <Box sx={{ my: 4 }}>
             <Typography variant="h4" component="h1" gutterBottom>
-              Top 5 Most Frequent Keyword From Last 7 Days
+              Top 10 Most Frequent Keywords From Last 7 Days
             </Typography>
+            <Typography variant="h5" component="h1" gutterBottom>
+              From {today} To {weekAgo}
+            </Typography>
+
             <BarChart
               width={640}
               height={400}
               data={words.data[0]}
-              fromDate={days-7}
+              fromDate={days - 7}
               toDate={days}
             />
           </Box>
@@ -37,7 +54,7 @@ const Submission = ({ words }) => {
 };
 
 export async function getServerSideProps() {
-  const res = await fetch("http://127.0.0.1:8000/word");
+  const res = await fetch("http://127.0.0.1:8000/noun");
   const words = await res.json();
 
   return {
