8309c86178add14f30e012c6eb81900208367f6f
Jonghyeok K <jka236@sfu.ca>
2022-06-16 22:30:40 -0700

    push to mongodb

--- scrap/word-count-beam/src/main/java/org/apache/beam/examples/MinimalWordCount.java
+++ scrap/word-count-beam/src/main/java/org/apache/beam/examples/MinimalWordCount.java
@@ -32,14 +32,15 @@ public class MinimalWordCount {
 
         // Create the Pipeline object with the options we defined above.
         Pipeline p = Pipeline.create(options);
-        Duration maxTime = Duration.standardMinutes(1);
+        Duration maxTime = Duration.standardMinutes(15);
 
         p.apply(KafkaIO.<Long, String>read()
                 .withBootstrapServers("localhost:9093")
                 .withTopic("article_title")
                 .withKeyDeserializer(LongDeserializer.class)
                 .withValueDeserializer(StringDeserializer.class)
-                // .updateConsumerProperties(ImmutableMap.of("receive.buffer.bytes", 1024 * 1024))
+                // .updateConsumerProperties(ImmutableMap.of("receive.buffer.bytes", 1024 *
+                // 1024))
                 // .updateConsumerProperties(ImmutableMap.of("auto.offset.reset",
                 // (Object)"earliest"))/
                 // .withMaxNumRecords(3)
@@ -48,14 +49,11 @@ public class MinimalWordCount {
         )
                 .apply(Values.<String>create())
                 .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
-                    String[] omitWords = { "to", "a", "an", "the", "but", "and", "to", "at",
-                            "in", "of", "for", "s", "at", "on", "as", "with",
-                            "the", "b", "a", "c" };
 
                     @ProcessElement
                     public void processElement(ProcessContext c) {
                         for (String word : c.element().split(TOKENIZER_PATTERN)) {
-                            if (!word.isEmpty() && !Arrays.asList(omitWords).contains(word.toLowerCase())) {
+                            if (!word.isEmpty()) {
                                 c.output(word.toLowerCase());
                             }
                         }
@@ -63,51 +61,59 @@ public class MinimalWordCount {
                 }))
                 .apply(Count.perElement())
                 .apply("ConvertToJson", MapElements.via(
-                    new SimpleFunction<KV<String, Long>, Document>() {
-                    public Document apply(KV<String, Long> input) {
-                        return Document.parse(String.format("{word : '%s', count: '%s', date: '%d'}",input.getKey(), 
-                                        input.getValue(), numberOfDays));
-                    }}))
-
-                // .apply(MapElements.into(TypeDescriptors.strings())
-                    // .via((KV<String, Long> wordCount) -> wordCount.getKey() + ": " + wordCount.getValue()))
-                    // .via((KV<String, Long> wordCount) -> String.format("{word : %s, count: %s, date: %s}",wordCount.getKey(), wordCount.getValue(), stringDate)))
-
-                // .apply("ExtractPayload",
-                // ParDo.of(new DoFn<String, KV<String, String>>() {
-                // @ProcessElement
-                // public void processElement(ProcessContext c)
-                // throws Exception {
-                // c.output(KV.of(String.format("{\"article_title\": \"test\"}"),
-                // String.format("{\"word\": \"%s\"}", c.element())));
-                // }
-                // }))
-
-                // .apply("FormatToWord", MapElements.via(new SimpleFunction<String, KV<String,
-                // String>>(){
-                // @Override
-                // public String apply(String token_word){
-                // // return String.format("{\"article_title\": \"%s\"}", token_word);
-                // return KV.of("article_title", token_word);
-                // }}
-                // ))
-
-                // .apply(TextIO.write().to("wordcounts"));
+                        new SimpleFunction<KV<String, Long>, Document>() {
+                            public Document apply(KV<String, Long> input) {
+                                return Document
+                                        .parse(String.format("{word : '%s', count: '%s', date: '%d'}", input.getKey(),
+                                                input.getValue(), numberOfDays));
+                            }
+                        }))
                 .apply(MongoDbIO.write()
-                    .withUri("mongodb+srv://rss_feed:rss_feed@cluster0.46sfz.mongodb.net/?retryWrites=true&w=majority")
-                    .withDatabase("rss_feed")
-                    .withCollection("rss_feed_write"));
-                // .apply(ElasticsearchIO.write().withConnectionConfiguration(
-                // ElasticsearchIO.ConnectionConfiguration.create(new String[]
-                // {"http://localhost:9200"}, "words", "words")));
-
-                // .apply(KafkaIO.<String, String>write()
-                //         .withBootstrapServers("localhost:9093")
-                //         .withTopic("rss_feed")
-                //         .withKeySerializer(StringSerializer.class)
-                //         .withValueSerializer(StringSerializer.class));
+                        .withUri(
+                                "mongodb+srv://rss_feed:rss_feed@cluster0.46sfz.mongodb.net/?retryWrites=true&w=majority")
+                        .withDatabase("rss_feed")
+                        .withCollection("rss_feed_write"));
 
         p.run().waitUntilFinish();
+
+        // .apply(MapElements.into(TypeDescriptors.strings())
+        // .via((KV<String, Long> wordCount) -> wordCount.getKey() + ": " +
+        // wordCount.getValue()))
+        // .via((KV<String, Long> wordCount) -> String.format("{word : %s, count: %s,
+        // date: %s}",wordCount.getKey(), wordCount.getValue(), stringDate)))
+
+        // .apply("ExtractPayload",
+        // ParDo.of(new DoFn<String, KV<String, String>>() {
+        // @ProcessElement
+        // public void processElement(ProcessContext c)
+        // throws Exception {
+        // c.output(KV.of(String.format("{\"article_title\": \"test\"}"),
+        // String.format("{\"word\": \"%s\"}", c.element())));
+        // }
+        // }))
+
+        // .apply("FormatToWord", MapElements.via(new SimpleFunction<String, KV<String,
+        // String>>(){
+        // @Override
+        // public String apply(String token_word){
+        // // return String.format("{\"article_title\": \"%s\"}", token_word);
+        // return KV.of("article_title", token_word);
+        // }}
+        // ))
+
+        // .apply(TextIO.write().to("wordcounts"));
+
+        // .apply(ElasticsearchIO.write().withConnectionConfiguration(
+        // ElasticsearchIO.ConnectionConfiguration.create(new String[]
+        // {"http://localhost:9200"}, "words", "words")));
+
+        // .apply(KafkaIO.<String, String>write()
+        // .withBootstrapServers("localhost:9093")
+        // .withTopic("rss_feed")
+        // .withKeySerializer(StringSerializer.class)
+        // .withValueSerializer(StringSerializer.class));
+
+        // p.run().waitUntilFinish();
         // p.run();
 
     }
