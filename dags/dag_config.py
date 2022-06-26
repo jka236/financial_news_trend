af3bcf2314c60252fec6d68ca278b99108a68025
Jonghyeok K <jka236@sfu.ca>
2022-06-26 12:53:08 -0700

    dag minor fix

--- dags/dag_config.py
+++ dags/dag_config.py
@@ -1,7 +1,6 @@
 class Config:
     RSS_FEED_LIST=[ 
                     "https://blog.feedspot.com/business_news_rss_feeds/", 
-                    "https://blog.feedspot.com/business_magazines/",
                     "https://blog.feedspot.com/canadian_news_rss_feeds/",
                     "https://blog.feedspot.com/world_news_rss_feeds/",
                     "https://blog.feedspot.com/usa_news_rss_feeds/"
